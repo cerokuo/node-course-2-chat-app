5fce4f51761c50cc290596092f5ceb98bac65496
NosyApp <ivan.blanco.martinez@gmail.com>
2017-02-21 18:30:06 -0500

    Added geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -25,6 +25,17 @@
 		jQuery('#messages').append(li);
 	});
 
+	socket.on('newLocationMessage', function (message) {
+		var li = jQuery('<li></li>');
+		var a = jQuery('<a target="_blank">My current location</a>');
+
+		li.text(`${message.from}: `);
+		a.attr('href', message.url);
+		li.append(a);
+		jQuery('#messages').append(li);
+	});
+
+// clicking the button message-form will launch this function
 	jQuery('#message-form').on('submit', function(e) {
 		e.preventDefault(); 
 
@@ -37,3 +48,21 @@
 		});
 	});
 	
+
+	//var to take the info from the button
+	var locationButton = jQuery('#send-location');
+
+	locationButton.on('click', function(e) {
+		if(!navigator.geolocation) {
+			return alert('Geolocation not supported by your browser');
+		}
+		// sending current location.
+		navigator.geolocation.getCurrentPosition(function(position) {
+			socket.emit('createLocationMessage', {
+				latitude: position.coords.latitude,
+				longitude: position.coords.longitude
+			});
+		}, function() {
+			alert('Unable to fetch location');
+		});
+	});
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,11 +1,20 @@
-var generateMessage = function(from, text) {
-	return  {
-		from,
-		text,
-		createdAt: new Date().getTime()
-	};
-
+var generateMessage = (from, text) => {
+  return {
+    from,
+    text,
+    createdAt: new Date().getTime()
+  };
 };
 
+var generateLocationMessage = (from, latitude, longitude) => {
+  return {
+    from,
+    url: `https://www.google.com/maps?q=${latitude},${longitude}`,
+    createdAt: new Date().getTime()
+  };
+};
 
-module.exports = {generateMessage};
+module.exports = {generateMessage, generateLocationMessage};
+
+
+
