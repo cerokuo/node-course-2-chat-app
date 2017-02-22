6ebb2563364a6f69f4c9b58f2748ff2347001449
NosyApp <ivan.blanco.martinez@gmail.com>
2017-02-22 11:36:29 -0500

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,25 @@
 	//store the socket in a variable and need to listen and send data from the server.
 	var socket = io();
 
+
+	function scrollToBottom() {
+		// Selectors
+		var messages = jQuery('#messages');
+
+		var newMessage = messages.children('li:last-child');
+		// Heights
+		var clientHeight = messages.prop('clientHeight');
+		var scrollTop = messages.prop('scrollTop');
+		var scrollHeight = messages.prop('scrollHeight');
+		var newMessageHeight = newMessage.innerHeight();
+		var lastMessageHeight = newMessage.prev().innerHeight();
+
+		if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+			messages.scrollTop(scrollHeight);
+		}
+	}
+
+
 	//log in chrome console when you have connected with the server
 	socket.on('connect', function() {
 		console.log('connected to the server');
@@ -29,6 +48,7 @@
 		});
 
 		jQuery('#messages').append(html);
+		scrollToBottom();
 	});
 
 	socket.on('newLocationMessage', function (message) {
@@ -41,6 +61,7 @@
 		});
 
 		jQuery('#messages').append(html);
+		scrollToBottom();
 	});
 
 // clicking the button message-form will launch this function
